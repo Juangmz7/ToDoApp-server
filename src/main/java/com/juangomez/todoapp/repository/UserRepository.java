b53e124d523c337bc55fa2ce8755a0321b5195c1
juan <juanjuangmo@gmail.com>
2025-07-07 20:13:13 +0200

    Repositories updated

--- src/main/java/com/juangomez/todoapp/repository/UserRepository.java
+++ src/main/java/com/juangomez/todoapp/repository/UserRepository.java
@@ -4,5 +4,7 @@ import com.juangomez.todoapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     boolean existsByUserName(String userName);
+    User findByUserName(String userName);
 }
