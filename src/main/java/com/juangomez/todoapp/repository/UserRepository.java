8ed57ecb6347e679f2b72e3407fc407166032150
juan <juanjuangmo@gmail.com>
2025-07-07 19:29:31 +0200

    existsByUsername implemented

--- src/main/java/com/juangomez/todoapp/repository/UserRepository.java
+++ src/main/java/com/juangomez/todoapp/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.juangomez.todoapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserByUserName(String userName);
+    boolean existsByUserName(String userName);
 }
