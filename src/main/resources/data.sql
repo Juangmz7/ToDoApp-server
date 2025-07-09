ced237ebbfcb10382ae416095d0f2fca5994c41a
juan <juanjuangmo@gmail.com>
2025-07-09 20:48:14 +0200

    vector_store created

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS vector_store (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    content text,
+    metadata json,
+    embedding vector(1536)
+    );
+
 -- Insert roles
 INSERT INTO roles (id, name) VALUES (1, 'ROLE_USER')
     ON CONFLICT (id) DO NOTHING;
