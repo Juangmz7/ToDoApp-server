6a2792e2c09883b84d4155d9d01a63e70bbbd945
juan <juanjuangmo@gmail.com>
2025-07-11 15:04:07 +0200

    Audio & Date format exception implemented

---
+++ src/main/java/com/juangomez/todoapp/config/exception/task/InvalidAudioFormatException.java
@@ -0,0 +1,7 @@
+package com.juangomez.todoapp.config.exception.task;
+
+public class InvalidAudioFormatException extends RuntimeException {
+    public InvalidAudioFormatException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/juangomez/todoapp/config/exception/task/NullDateException.java
@@ -0,0 +1,8 @@
+package com.juangomez.todoapp.config.exception.task;
+
+public class NullDateException extends RuntimeException {
+    public NullDateException(String message) {
+        super(message);
+    }
+
+}
