4b8a58d60c6e33fa25c20bfffc012019dc08ebe5
juan <juanjuangmo@gmail.com>
2025-07-11 14:54:03 +0200

    Dependencies modifications

--- src/main/java/com/juangomez/todoapp/config/SecurityConfig.java
+++ src/main/java/com/juangomez/todoapp/config/SecurityConfig.java
@@ -1,14 +1,9 @@
 package com.juangomez.todoapp.config;
 
-import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
