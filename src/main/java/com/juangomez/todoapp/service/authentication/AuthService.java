b6cd393c127b6945f9d6cefc6b21c761b89f00b8
juan <juanjuangmo@gmail.com>
2025-07-14 01:23:46 +0200

    logout & forgot password implemented

--- src/main/java/com/juangomez/todoapp/service/authentication/AuthService.java
+++ src/main/java/com/juangomez/todoapp/service/authentication/AuthService.java
@@ -1,8 +1,10 @@
 package com.juangomez.todoapp.service.authentication;
 
+import com.juangomez.todoapp.dto.EmailRequest;
 import com.juangomez.todoapp.dto.LoginRequest;
 import com.juangomez.todoapp.dto.UserRegisterRequest;
 import com.juangomez.todoapp.dto.UserResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 import javax.management.relation.RoleNotFoundException;
@@ -12,4 +14,12 @@ public interface AuthService {
     String login (LoginRequest user);
 
     UserResponse register(UserRegisterRequest user) throws RoleNotFoundException;
+
+    void forgotPassword(EmailRequest emailRequest);
+
+    void logout(HttpServletRequest request);
+
+    void resetPasswordTokenValidation(String token);
+
+    void changePassword(String password, String token);
 }
--- src/main/java/com/juangomez/todoapp/serviceimpl/authentication/AuthServiceImpl.java
+++ src/main/java/com/juangomez/todoapp/serviceimpl/authentication/AuthServiceImpl.java
@@ -1,7 +1,8 @@
 package com.juangomez.todoapp.serviceimpl.authentication;
 
-import com.juangomez.todoapp.config.SecurityConfig;
+import com.juangomez.todoapp.config.authentication.SecurityConfig;
 import com.juangomez.todoapp.config.exception.user.*;
+import com.juangomez.todoapp.dto.EmailRequest;
 import com.juangomez.todoapp.dto.UserRegisterRequest;
 import com.juangomez.todoapp.dto.UserResponse;
 import com.juangomez.todoapp.model.Role;
@@ -9,9 +10,12 @@ import com.juangomez.todoapp.model.User;
 import com.juangomez.todoapp.model.enums.RoleName;
 import com.juangomez.todoapp.repository.RoleRepository;
 import com.juangomez.todoapp.repository.UserRepository;
+import com.juangomez.todoapp.service.MailService;
 import com.juangomez.todoapp.service.authentication.JwtService;
 import com.juangomez.todoapp.service.authentication.AuthService;
 import com.juangomez.todoapp.dto.LoginRequest;
+import com.juangomez.todoapp.service.authentication.TokenBlacklistService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,8 +24,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.management.relation.RoleNotFoundException;
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 
 @Service
@@ -38,6 +44,12 @@ public class AuthServiceImpl implements AuthService {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenBlacklistService tokenBlacklistService;
+
+    @Autowired
+    private MailService mailService;
 
     @Override
     public String login(LoginRequest request) {
@@ -103,5 +115,97 @@ public class AuthServiceImpl implements AuthService {
                 user.getPassword(),
                 user.getEmail()
         );
+    }
+
+    @Override
+    public void forgotPassword(EmailRequest emailRequest) {
+        // Validates the email
+        if (!EmailValidator.getInstance().isValid(emailRequest.getEmail())) {
+            throw new InvalidEmailException("Invalid email format");
+        }
+
+        User user = userRepository.findByEmail(emailRequest.getEmail());
+
+        // If it does not exist in db
+        if (user == null) {
+            return;
+        }
+
+        // Create the token
+        String token = UUID.randomUUID().toString();
+
+        // To search by token or username
+        tokenBlacklistService.addToken(user.getUsername(), token, Duration.ofMinutes(15));
+        tokenBlacklistService.addToken(token, user.getUsername(), Duration.ofMinutes(15));
+
+        // Send the email
+        String url = "https://localhost:8081/auth/validate-reset-token?token=" + token;
+        mailService.sendForgotPasswordMail(token,"juanjuango@hotmail.com" , url);
+
+    }
+
+    @Override
+    public void logout(HttpServletRequest request) {
+        String token = jwtService.extractAuthToken(request);
+        // The token is not valid
+        if (token == null) {
+            return;
+        }
+        // Add the token to the Blacklist
+        tokenBlacklistService.blacklistToken(token, jwtService.tokenTtl(token));
+    }
+
+    @Override
+    public void resetPasswordTokenValidation(String token) {
+
+        // If the token is in db, then it is valid
+        if (!tokenBlacklistService.isTokenValid(token)) {
+            throw new IllegalStateException("The token provided is expired");
+        }
+
+        // Verify if the token is the last one sent
+        String username = tokenBlacklistService.getObject(token);
+
+        /*
+            When a new token it's provided, last one is overridden.
+            In db there are 2 objects for each request:
+
+                - Key(token) : username -> This one is never overwritten
+                - Key(Username) : token -> This one is
+
+            The point it's to prove that the one which is override it's equal to
+            the token provided in actual request
+
+            This will verify it is the last generated one
+        */
+        if (!tokenBlacklistService.getObject(username).equals(token)) {
+            throw new IllegalStateException("The token provided is not valid");
+        }
+    }
+
+    @Override
+    public void changePassword(String password, String token) {
+
+        // Token verification
+        resetPasswordTokenValidation(token);
+
+        String username = tokenBlacklistService.getObject(token);
+
+        if (username == null) {
+            return;
+        }
+
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            return;
+        }
+
+        // Password change and update
+        String encryptedPassword = SecurityConfig.bCryptPasswordEncoder.encode(password);
+        user.setPassword(encryptedPassword);
+
+        userRepository.save(user);
+
     }
 }
