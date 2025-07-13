8773bffd20f20a327ed7d4f217b97364ca504ee2
juan <juanjuangmo@gmail.com>
2025-07-14 01:20:31 +0200

    MailService implemented

---
+++ src/main/java/com/juangomez/todoapp/service/MailService.java
@@ -0,0 +1,11 @@
+package com.juangomez.todoapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MailService {
+
+    // Sends an email with a body template for forgot-password
+    void sendForgotPasswordMail(String token, String email, String url);
+
+}
---
+++ src/main/java/com/juangomez/todoapp/serviceimpl/MailServiceIml.java
@@ -0,0 +1,51 @@
+package com.juangomez.todoapp.serviceimpl;
+
+import com.juangomez.todoapp.config.exception.user.InvalidEmailException;
+import com.juangomez.todoapp.service.MailService;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceIml implements MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void sendForgotPasswordMail(String token, String email, String link) {
+        if (token.isEmpty() || token.trim().isEmpty()) {
+            return;
+        }
+        if (email.isEmpty() || email.trim().isEmpty()) {
+            return;
+        }
+
+        String mailBody = """
+                <p>Hi!</p>
+                <p>Click the link below to change your password:</p>
+                <p><a href="%s">Reset your password</a></p>
+                <p>If it wasn't you, please contact us at 666 66 66 66</p>
+                <p>Cheers!</p>
+        """.formatted(link);
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+
+        try {
+            helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(email);
+            helper.setSubject("Reset your password");
+            helper.setText(mailBody, true);
+        } catch (Exception e) {
+            throw new InvalidEmailException("Wrong email");
+        }
+
+        System.out.println(mailBody);
+
+        mailSender.send(message);
+    }
+}
